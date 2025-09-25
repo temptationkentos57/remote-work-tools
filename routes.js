9753e8970d34f5155a29732840b2c1f18ce9cfca
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-25 16:48:02 +0200

    Updated chat message for clarity

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Đường dẫn đến tính năng nhắn tin
 router.get('/chat', (req, res) => {
-  res.send('Chức năng nhắn tin sẽ ở đây.');
+  res.send('Chức năng nhắn tin sẽ được triển khai tại đây.');
 });
 
 module.exports = router;
