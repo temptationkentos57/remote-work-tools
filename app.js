c1ad10823f0c6ae81112affe7f1ec39f485b9dd9
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-25 16:48:59 +0200

    Update greeting message to include English translation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Xin chào, đây là dịch vụ Remote Work Tools!');
+    res.send('Xin chào, đây là dịch vụ Remote Work Tools! (Hello, this is the Remote Work Tools service!)');
 });
 
 app.listen(PORT, () => {
