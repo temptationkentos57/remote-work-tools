17d9238dc394be5a7090ece80518030eb1412f60
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-25 16:47:19 +0200

    Fix indentation in app.js for better readability.

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Xin chào, đây là dịch vụ Remote Work Tools!');
+    res.send('Xin chào, đây là dịch vụ Remote Work Tools!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy trên http://localhost:${PORT}`);
+    console.log(`Server đang chạy trên http://localhost:${PORT}`);
 });
