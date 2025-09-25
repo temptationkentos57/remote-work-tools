cac068e41aba0503579cf2ac1f9c1c958a985eff
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-25 16:48:45 +0200

    Update chat route response message to be more descriptive.

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Đường dẫn đến tính năng nhắn tin
 router.get('/chat', (req, res) => {
-  res.send('Chức năng nhắn tin sẽ được triển khai tại đây.');
+  res.send('Chức năng nhắn tin đang được phát triển, vui lòng quay lại sau.');
 });
 
 module.exports = router;
