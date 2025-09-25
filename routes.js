fde2a5fa61e813192a740cb957a3dc7027689ab5
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-25 16:49:33 +0200

    Update chat message to improve user feedback.

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Đường dẫn đến tính năng nhắn tin
 router.get('/chat', (req, res) => {
-  res.send('Chức năng nhắn tin đang được phát triển, vui lòng quay lại sau.');
+  res.send('Chức năng nhắn tin đang được phát triển. Chúng tôi cảm ơn sự kiên nhẫn của bạn!');
 });
 
 module.exports = router;
